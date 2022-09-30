d05a742e755dbe633dbca8729ff3b930c3b97f06
zandoh <zclark@Zacs-MacBook-Pro.local>
2022-09-29 22:44:14 -0400

    Remove log

--- src/hooks/onClickOutside.tsx
+++ src/hooks/onClickOutside.tsx
@@ -7,9 +7,7 @@ import { RefObject, useEffect } from "react";
 export const useOnClickOutside = (ref: RefObject<any>, callback: Function) => {
   useEffect(() => {
     const handleClickOutside = (event: Event) => {
-      console.log("clicked! ", ref);
       if (ref.current && !ref.current.contains(event.target)) {
-        console.log("calling callback");
         callback();
       }
     };
