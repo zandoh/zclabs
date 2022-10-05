f6ab01e74891a4820e4f5c712588b9ee97cd5857
zandoh <zclark@Zacs-MacBook-Pro.local>
2022-10-04 21:31:41 -0400

    Aside gradient direction

--- src/components/Page/Aside/Aside.tsx
+++ src/components/Page/Aside/Aside.tsx
@@ -12,7 +12,7 @@ export default function Aside({
 }: HTMLAttributes<HTMLDivElement>) {
   return (
     <aside
-      className="float-left inline-block h-[80vh] min-h-[auto] w-full max-w-sm overflow-auto rounded-tl-[32px] rounded-bl-[32px] bg-gradient-to-r from-[color:#21e1ae] via-[color:#09aeea] to-[color:#09aeea] px-[30px] pt-[70px] pb-[45px] text-center dark:bg-[color:var(--zc-color-base)] dark:from-transparent dark:via-transparent dark:to-transparent"
+      className="float-left inline-block h-[80vh] min-h-[auto] w-full max-w-sm overflow-auto rounded-tl-[32px] rounded-bl-[32px] bg-gradient-to-b from-[color:#21e1ae] via-[color:#09aeea] to-[color:#09aeea] px-[30px] pt-[70px] pb-[45px] text-center dark:bg-[color:var(--zc-color-base)] dark:from-transparent dark:via-transparent dark:to-transparent"
       {...rest}
     >
       <AsideImage />
