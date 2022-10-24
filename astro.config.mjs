dd68fd55b03036ee741529da1ef52500f6c90e0d
zandoh <zclark@Zacs-MacBook-Pro.local>
2022-10-23 22:21:08 -0400

    Reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
-});
+export default defineConfig({});
--- src/components/Aside/AsideCopyright.tsx
+++
@@ -1,9 +0,0 @@
-const AsideCopyright = () => {
-  return (
-    <div className="absolute bottom-0 left-0 w-[380px] px-[10px] pt-[5px] pb-[15px] text-center text-xs leading-3 text-[#bbb]">
-      © {new Date().getFullYear()} zclabs.
-    </div>
-  );
-};
-
-export default AsideCopyright;
