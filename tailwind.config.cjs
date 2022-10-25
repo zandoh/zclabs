b5d9c6668591f2b684f69605e967d54250ac5741
zandoh <zclark@Zacs-MacBook-Pro.local>
2022-10-24 22:15:43 -0400

    next-tui: Experience and Education sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
