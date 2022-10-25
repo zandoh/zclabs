e0b0de3e65383a9892848071d901aa2e53177b27
zandoh <zclark@Zacs-MacBook-Pro.local>
2022-10-24 20:54:56 -0400

    next-tui: initial commit -- nav and intro section

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
