fd6d4cc183bc07958fec12bfac2d4e473586125a
zandoh <zandoh9@gmail.com>
2023-03-29 22:27:17 -0400

    Remove accessible template, use tailwind and baseline theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), tailwind()],
+  integrations: [compress(), tailwind(), react()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
