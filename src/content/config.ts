3cf2ba4305cba81997575eab672950a9d1110fca
zandoh <zandoh9@gmail.com>
2023-09-02 01:17:50 -0400

    Projects stub

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const articlesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+export const collections = {
+  articles: articlesCollection,
+};
