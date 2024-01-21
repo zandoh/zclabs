7f3e85e217bd25dd6b4b91e0a7fe3a719a3c7dac
zandoh <zandoh9@gmail.com>
2024-01-21 01:36:14 -0500

    Integrate images into data content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,19 @@ import { defineCollection, z } from "astro:content";
 
 const projects = defineCollection({
   type: "data",
-  schema: z.object({
-    name: z.string(),
-    description: z.string(),
-    logo: z.string(),
-    link: z.object({
-      href: z.string(),
-      label: z.string(),
-    }),
-  }),
+  schema: ({ image }) =>
+    z.array(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        logo: z.object({ image: image() }),
+        link: z.object({
+          href: z.string(),
+          label: z.string(),
+        }),
+        featured: z.boolean(),
+      }),
+    ),
 });
 
 const favorites = defineCollection({
@@ -62,16 +66,17 @@ const navigation = defineCollection({
 
 const roles = defineCollection({
   type: "data",
-  schema: z.array(
-    z.object({
-      company: z.string(),
-      title: z.string(),
-      logo: z.union([z.literal("bryxLogo"), z.literal("dattoLogo"), z.literal("edtLogo"), z.literal("cxtecLogo")]),
-      start: z.string().datetime(),
-      end: z.string().datetime().nullable(),
-      current: z.boolean(),
-    }),
-  ),
+  schema: ({ image }) =>
+    z.array(
+      z.object({
+        company: z.string(),
+        title: z.string(),
+        logo: z.object({ image: image() }),
+        start: z.string().datetime(),
+        end: z.string().datetime().nullable(),
+        current: z.boolean(),
+      }),
+    ),
 });
 
 export const collections = {
