c56657548abf29305821215e22e41c671d17aaa2
zandoh <zandoh9@gmail.com>
2024-01-20 22:03:46 -0500

    Make everything dynamic, leverage content collections

--- src/content/articles/test.md
+++
@@ -1,7 +0,0 @@
----
-# @TODO https://docs.astro.build/en/guides/content-collections/#migrating-from-file-based-routing
-
-title: "Test 1"
-description: "Description description description"
-date: "1994-07-21"
----
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,69 @@
 import { defineCollection, z } from "astro:content";
 
-const articlesCollection = defineCollection({
-  type: "content",
+const projects = defineCollection({
+  type: "data",
   schema: z.object({
-    title: z.string(),
+    name: z.string(),
     description: z.string(),
-    date: z.string(),
+    logo: z.string(),
+    link: z.object({
+      href: z.string(),
+      label: z.string(),
+    }),
   }),
 });
 
+const favorites = defineCollection({
+  type: "data",
+  schema: z.object({
+    heading: z.string(),
+    data: z.array(
+      z.object({
+        type: z.union([z.literal("artist"), z.literal("team"), z.literal("podcast"), z.literal("tv")]),
+        name: z.string(),
+        link: z.string(),
+      }),
+    ),
+  }),
+});
+
+const socials = defineCollection({
+  type: "data",
+  schema: z.object({
+    iconName: z.union([z.literal("linkedin"), z.literal("github"), z.literal("twitter")]),
+    label: z.string(),
+    link: z.string(),
+  }),
+});
+
+const uses = defineCollection({
+  type: "data",
+  schema: z.object({
+    section: z.string(),
+    data: z.array(
+      z.object({
+        name: z.string(),
+        link: z.string(),
+        description: z.string(),
+      }),
+    ),
+  }),
+});
+
+const navigation = defineCollection({
+  type: "data",
+  schema: z.array(
+    z.object({
+      route: z.string(),
+      title: z.string(),
+    }),
+  ),
+});
+
 export const collections = {
-  articles: articlesCollection,
+  projects,
+  favorites,
+  socials,
+  uses,
+  navigation,
 };
