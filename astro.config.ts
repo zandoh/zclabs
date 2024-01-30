783c7924d5b5f10d39c39b3f254c3c4c6f01e3f5
zandoh <zandoh9@gmail.com>
2024-01-29 22:53:22 -0500

    Image optimization

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
+import { imageService } from "@unpic/astro/service";
 import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
@@ -11,8 +12,13 @@ export default defineConfig({
     webAnalytics: {
       enabled: true,
     },
-    imageService: true,
   }),
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+      layout: "constrained",
+    }),
+  },
   integrations: [tailwind(), react(), compress()],
   publicDir: "./public",
 });
